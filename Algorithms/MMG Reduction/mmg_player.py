53fd089c9d986b232ec83b72f7776d3869542b4b
Guy <guywiz@gmail.com>
2024-03-21 10:28:26 +0100

    Update mmg_player.py
    Updated player with methods allowing to update edge weights

--- Algorithms/MMG Reduction/mmg_player.py
+++ Algorithms/MMG Reduction/mmg_player.py
@@ -1,27 +1,15 @@
 from tulip import *
-from mmg_ring import merge_operator, contract_operator
+from mmg_ring import *
+from agree import *
 
 class MMG_player(object):
 	"""
-	This class implements a reduction algorithm first published in:
+	This piece of code is written as to make it usable within
+	the Graph Visualization Framework Tulip, alongside the graph.
+	It is assumed the script is applied to a pair of graphs obtained
+	from the reduction algorithm.
 	
-	Bruno Pinaud, Maud Bénichou, Guy Melançon.
-	Extraction d'un réseau social criminel par transformation d'un graphe d'enquête multivarié.
-	Conférence Extraction et Gestion des Connaissances (EGC) 2023, Revue RNTI-039. pp.151-162.
-	Référence HAL hal-03929950.
-
-	The algorithm computes a social network out of a multivariate multigraph, by iteratively
-	merging multiple edges and contracting simple paths connecting entitites of a given type.
-	By default, the type the multivariate graph is reduced to is PERSON, hence producing a social network
-	between individuals connected through multiple artefact.
-
-	This work was motivated by the need to consider a social network of actors
-	starting from a series of observation collected along a criminal investigations.
-	Links in the multivariate graph typically connect people to phones or app logins, to vehicle
-	and physical location.
-
-	This piece of code is written as to make it usable within the Graph Visualization Framework Tulip,
-	alongisde the graph. The historicize method then helps mapping edges of the social network back
+	The historicize method then helps mapping edges of the social network back
 	into the multivariate graph.
 	"""
 	def __init__(self, graph, projected_type, weight_property_name):
@@ -35,6 +23,13 @@ class MMG_player(object):
 		self.weight = self.graph.getDoubleProperty(weight_property_name)
 		self.result_graph = graph.getSubGraph(f'Projected graph {self.projected_type}')
 		self.original_multivariate_graph = graph.getSubGraph('Original multivariate graph')
+		self.ring = MMG_ring()
+
+	def reset_ring_atomic_values(self):
+		edges_values = {}
+		for e in self.original_multivariate_graph.getEdges():
+			edges_values[self.item_id[e]] = self.weight[e]
+		self.ring.set_atomic_values(edges_values)
 
 	def historicize(self):
 		# we assume an edge has been selected in the social network
@@ -51,8 +46,12 @@ class MMG_player(object):
 			for id in history_edge_ids:
 				e = self.find_edge(id)
 				self.original_multivariate_graph['viewSelection'][e] = True
-		sub = self.original_multivariate_graph.inducedSubGraph(self.original_multivariate_graph['viewSelection'])
+				self.original_multivariate_graph['viewSelection'][self.original_multivariate_graph.source(e)] = True
+				self.original_multivariate_graph['viewSelection'][self.original_multivariate_graph.target(e)] = True
+		sub = self.original_multivariate_graph.addSubGraph(self.original_multivariate_graph['viewSelection'])
 		sub.setName(f'Expand edge {history_edge_ids}')
+		sub['viewSelection'].setAllNodeValue(False)
+		sub['viewSelection'].setAllEdgeValue(False)
 		
 	def find_edge(self, edge_id):
 		edge_ids = self.original_multivariate_graph.getStringProperty('id')
@@ -61,7 +60,31 @@ class MMG_player(object):
 				return e
 		return None
 
-	def reload_weights(self, weight_file):
+	def report_type_weights(self):
+		weight_dict = {}
+		for e in self.original_multivariate_graph.getEdges():
+			weight_dict[self.types[e]] = self.weight[e]
+		return sorted(weight_dict.items(), key=lambda x: -x[1])
+
+
+	def reload_weights(self, weight_dict):
+		'''
+		This method allows to reset weights assigned to primary edge types
+		(edges that are part of the original multivariate multigraph).
+		New weights are read from key-value pairs (type - float).
+		Types that do not appear in the dict keep their already assigned weights.
+		'''
+		types_new_weight = weight_dict.keys()
+		for e in self.original_multivariate_graph.getEdges():
+			if self.types[e] in types_new_weight:
+				self.weight[e] = weight_dict[self.types[e]]
+
+	def recompute_weights(self):
+		for e in self.result_graph.getEdges():
+			new_value = self.ring.eval(self.compute_history[e])
+			self.weight[e] = new_value
+
+	def reload_weights_from_file(self, weight_file):
 		'''
 		This method allows to reset weights assigned to primary edge types
 		(edges that are part of the original multivariate multigraph)
@@ -78,10 +101,15 @@ class MMG_player(object):
 				weight_dict[row['type']] = float(row['value'])
 			for e in self.original_multivariate_graph.getEdges():
 				self.weight[e] = weight_dict[self.types[e]]
-		for e in self.result_graph.getEdges():
-			self.weight[e] = self._recompute_weight_(self.compute_history[e])
 				
 def main(graph):
+	'''
+	The script must be run from the superGraph holding the two
+	graphs: social network as well as the original multigraph.
+	'''
 	player = MMG_player(graph, 'PERSON', 'weight')
 	player.historicize()
+
+	
+
 
