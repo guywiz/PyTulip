1fcde81a178b125bff685643fa9c6a591b5a61b4
Guy <guywiz@gmail.com>
2024-05-30 14:12:25 +0200

    Update BrokerScore
    Typos essentially

--- Algorithms/BrokerScore/src/BrokerScore.py
+++ Algorithms/BrokerScore/src/BrokerScore.py
@@ -7,9 +7,6 @@ import tulipplugins
 import leidenalg
 import igraph
 
-# local package
-from Dijkstra import *
-
 """
 This script offers an alternative to Paquet-CLouston an Bouchard `python` package
 computing their broker score defined on node sof a network. Please refer to their paper
@@ -32,9 +29,58 @@ a series of subgraph and makes use of unrobust naming conventions.
 """
 
 
+class Dijkstra(object):
+    """
+    computes distances from a source using Dijkstra's algorithm
+    optionally displays shortest path between two node
+
+    the given graph is assumed to be connected
+    """
+
+    def __init__(self, graph):
+        """
+        stores the graph
+        instantiates the necessary boolean properties
+        """
+        self.graph = graph
+        self.source = None
+        self.dist_to_source = self.graph.getDoubleProperty("dist_to_source")
+        self.infinity = self.graph.numberOfNodes()
+        self.parent = {}
+        self.nodeList = [n for n in self.graph.getNodes()]
+
+    def compute_distance_from(self, source):
+        """
+        distances from the given node are computed
+        """
+        for n in self.graph.getNodes():
+            self.dist_to_source[n] = self.infinity
+            self.parent[n] = None
+        self.dist_to_source[source] = 0.0
+
+        while len(self.nodeList) > 0:
+            u = self.__minNode__()
+            for v in self.graph.getInOutNodes(u):
+                tmp_dist = self.dist_to_source[u] + self.dist_between(u, v)
+                if tmp_dist < self.dist_to_source[v]:
+                    self.dist_to_source[v] = tmp_dist
+                    self.parent[v] = u
+
+    def __minNode__(self):
+        values = list(map(lambda x: self.dist_to_source[x], self.nodeList))
+        min_value = min(values)
+        min_node = self.nodeList.pop(values.index(min_value))
+        return min_node
+
+    def dist_between(self, u, v):
+        """
+        nodes u and v are assumed to be neighbor nodes
+        """
+        return 1
+
 class iGraphConverter:
     """
-    Utility class to convert grpahs between formats,
+    Utility class to convert graphs between formats,
     baiscally Tulip and iGraph
     (used by the iGrpah implementation of the Leiden algorithm)
     """
@@ -204,7 +250,11 @@ class BrokerScore(tlp.DoubleAlgorithm):
             )
             membership = communities.membership
             for n in self.graph.getNodes():
-                self.community[n] = membership[gc.node_to_index[n]]
+                try:
+                    self.community[n] = membership[gc.node_to_index[n]]
+                except TypeError:
+                    print('Node to index')
+                    print(gc.node_to_index)
 
     def collect_community_names(self):
         self.community_names = sorted(
